dc604d77143532b1df70c279582cdee792091d77
Aflah Riswan <riswanaflah@gmail.com>
2026-09-15 11:29:00 +0530

    added interface for user repository

---
+++ user-service/src/interface/repositories/IUserRepositories.ts
@@ -0,0 +1,8 @@
+import { CreateUserData, User } from "../../domain/entities/User";
+
+export interface IUserRepositories {
+    findById (id : string) : Promise<User>,
+    create (user : CreateUserData) : Promise<User>,
+    findByEmail(email : string) : Promise<User>,
+    update(user : Partial<User>) : Promise<User>
+}
